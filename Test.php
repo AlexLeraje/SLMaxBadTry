51a6d79a4f6343963c5af7eb6b90e075b8c1f054
AlexLeraje <alex.leraje@gmail.com>
2023-07-27 04:09:54 +0300

    Add сode examples

--- Test.php
+++ Test.php
@@ -4,8 +4,12 @@ use Database\HumansStore;
 
 class Test {
     public function __construct() {
+
+        //Инициализируем хранилище Store/Store.json
+        //Структура БД по сути описывается в классе Database/HumanItem.php
         $humansStore = new HumansStore('Store');
 
+        //Добавление человека
         $humansStore->add(
             'Андрей',
             'Власов',
@@ -14,15 +18,32 @@ class Test {
             'Минск',
         );
 
+        //Поиск по людям, количество where может быть бесконечным
+        //При каждом вызове where ищет относительно предыдущего вызова where
         print_r($humansStore->search()->where('id', '<=', 2)->where('id', '>', 1)->get());
+
+        //Получаем всех людей
         print_r($humansStore->search()->get());
 
-        $humansStore->search()->where('id', '=', 5)->delete();
+        //Удаляем по списку всех людей с id > 5, условия могут быть любыми
+        $humansStore->search()->where('id', '>=', 5)->delete();
 
+        //Удаление пользователя по id
+        $humansStore->delete(5);
+
+        //Пример вывода данных
+        $out = [];
         foreach($humansStore->search()->get() AS $value) {
-            echo $value->getAge();
-            echo $value->getSex();
-            echo '<br/>';
+            $out[] = [
+                'id' => $value->id,
+                'Имя' => $value->name,
+                'Фамилия' => $value->surname,
+                'Возраст' => $value->getAge(), //Получаем возраст
+                'Пол' => $value->getSex(), //Получаем пол
+                'Город' => $value->city,
+            ];
         }
+
+        print_r($out);
     }
 }
